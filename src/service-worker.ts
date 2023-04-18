3433c94592cb3546b0010af0758a2a799b245948
Danko Aleksejevs <danko@very.lv>
2023-04-18 18:06:53 +0300

    Run init on every service worker spinup

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -2,22 +2,24 @@
 
 import { getSettings, initSettings } from './settings.js';
 import { clearCache, clearMetadata, initStorage } from './storage.js';
-import { packageVersion } from './util.js';
+import { packageVersion, reportAndNull } from './util.js';
 
 import { maybeClearContextCache } from './remapping/context.js';
 import { setUpAPIPort } from './api/impl.js';
 import { asChrome } from './browsers/any.js';
 import { updateChromeConfig } from './browsers/chrome.js';
 
+let initRan = false;
 async function init(installed: boolean) {
 	
 	console.log('init', packageVersion);
 	
-	await initStorage();
-	
-	await initSettings(updateChromeConfig);
-	
-	await maybeClearContextCache();
+	if (!initRan) {
+		initRan = true;
+		await initStorage();
+		await initSettings(updateChromeConfig);
+		await maybeClearContextCache();
+	}
 	
 	if (installed && !getSettings().instances.length) asChrome.runtime.openOptionsPage();
 	
@@ -25,6 +27,7 @@ async function init(installed: boolean) {
 
 asChrome.runtime.onStartup.addListener(() => init(false));
 asChrome.runtime.onInstalled.addListener(() => init(true));
+init(false).catch(reportAndNull);
 
 asChrome.runtime.onMessage.addListener(async (message) => {
 	if (typeof message != 'object') return;
